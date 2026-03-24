bca34dd15a0250095c688e23d7b7ed19ae6fd2dd
Gustavo Rael <gu1603rael@gmail.com>
2026-03-24 17:46:13 -0300

    alterei qual seja renderizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Cadastrar from './components/Cadastrar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Cadastrar />
   </StrictMode>
 )
